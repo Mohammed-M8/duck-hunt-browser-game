17cb8f641280a92fe030fd683b98e5d2fa6558ef
Mohammed-M8 <202200692@student.polytechnic.bh>
2026-08-05 10:29:05 +0300

    fixed image paths

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 /*---------- Variables (state) ---------*/
 let renderId, createId, flapId, speedId;
 /*-------------- Constants -------------*/
-const shotSound = new Audio("../assets/Sounds/Shot.mp3")
-const winSound = new Audio("../assets/Sounds/Win.wav")
-const lossSound = new Audio("../assets/Sounds/Loss.wav")
+const shotSound = new Audio("./assets/Sounds/Shot.mp3")
+const winSound = new Audio("./assets/Sounds/Win.wav")
+const lossSound = new Audio("./assets/Sounds/Loss.wav")
 
 const render = () => {
     remainSpan.textContent = game.lives
@@ -39,7 +39,6 @@ const game = {
         this.element.replaceChildren()
         this.lives = 10
         this.played = true
-        console.log("game started")
         render();
 
         speedId = setInterval(faster, 5000)
@@ -98,9 +97,9 @@ const renderTimer = () => {
 }
 
 const types = [
-    { name: "duck", flap1: "../assets/duckEnemy-1.png", flap2: "../assets/duckEnemy-2.png", weight: 70, speed: 10 },
-    { name: "crow", flap1: "../assets/crowChill-1.png", flap2: "../assets/crowChill-2.png", weight: 20, speed: 12 },
-    { name: "eagle", flap1: "../assets/eagleChill-1.png", flap2: "../assets/eagleChill-2.png", weight: 10, speed: 14 },
+    { name: "duck", flap1: "./assets/duckEnemy-1.png", flap2: "./assets/duckEnemy-2.png", weight: 70, speed: 10 },
+    { name: "crow", flap1: "./assets/crowChill-1.png", flap2: "./assets/crowChill-2.png", weight: 20, speed: 12 },
+    { name: "eagle", flap1: "./assets/eagleChill-1.png", flap2: "./assets/eagleChill-2.png", weight: 10, speed: 14 },
 ]
 const createBird = () => {
 
@@ -164,7 +163,7 @@ const shot = (e) => {
     const birdBox = e.currentTarget;
     const birdImg = birdBox.querySelector(".bird");
 
-    birdImg.src = "../assets/duckEnemy-3.png";
+    birdImg.src = "./assets/duckEnemy-3.png";
 
     const bird = birdBox.bird;
 
@@ -197,7 +196,6 @@ const shot = (e) => {
     }
     setTimeout(render, 500)
     if (game.score === game.targetScore) end()
-    console.log(game.score);
 
 
 
@@ -209,7 +207,6 @@ const shot = (e) => {
 
 
 const end = () => {
-    console.log("GameOver");
     game.win = game.timer >= 0 && (game.targetScore === game.score)
     if (game.win) {
         resultSpan.textContent = "YOU WON!"
@@ -238,7 +235,7 @@ game.element.addEventListener('click', (e) => {
     shotSound.currentTime = 0;
     shotSound.play()
     const shotSmoke = document.createElement('img')
-    shotSmoke.src = "../assets/shot.png"
+    shotSmoke.src = "./assets/shot.png"
     shotSmoke.classList.add("shot")
     if (game.lives === 0) end()
 
